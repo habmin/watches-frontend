038961f45bd16dc1b99635a7fb2c522a0cba671b
Jasmine Snow <jasminesimmone@gmail.com>
2020-11-20 17:50:23 -0800

    working on cart functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './App.css';
 import NewProduct from './components/NewProduct.jsx';
 import ShowProduct from './components/ShowProduct.jsx';
 import ProductEdit from './components/ProductEdit.jsx';
@@ -6,7 +7,7 @@ import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 
 let baseURL;
 if (process.env.NODE_ENV === 'development')
-  baseURL = 'http://localhost:3005';
+  baseURL = 'http://localhost:3003';
 else
   baseURL = 'heroku/depploment URL placehorder';
 
@@ -14,9 +15,24 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      products: []
+      products: [],
+      cartItems: [],
     }
   }
+  cartFunction = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    let alreadyInCart = false
+    cartItems.forEach((item ) => {
+      if (item._id === product._id) {
+        item.count++;
+        alreadyInCart = true;
+      }
+    })
+    if(!alreadyInCart) {
+      cartItems.push({...product, count: 1})
+    }
+    this.setState({ cartItems });
+  };
 
   getProducts = async () => {
     try{
@@ -33,11 +49,9 @@ class App extends Component {
       console.log(error);
     }
   };
-
   componentDidMount() {
     this.getProducts();
   };
-
   addProduct = (newProduct) => {
     const productsBuffer = [...this.state.products];
     productsBuffer.push(newProduct);
@@ -63,11 +77,11 @@ class App extends Component {
       products: productsBuffer
     });
   }
-
   render() {
     return (
       <div className="App">
         <Router>
+          <div>
           <div className="header">
             {
             /* make this a nav bar
@@ -82,9 +96,9 @@ class App extends Component {
               this.state.products.map((product) => {
                 return (
                   <Route path={"/" + product._id + "/edit"}>
-                    <ProductEdit 
-                      baseURL={baseURL} 
-                      product={product} 
+                    <ProductEdit
+                      baseURL={baseURL}
+                      product={product}
                       updateProduct={this.updateProduct}
                       deletedProduct={this.deletedProduct}/>
                   </Route>
@@ -103,22 +117,29 @@ class App extends Component {
             <Route path='/new'>
               <NewProduct baseURL={baseURL} addProduct={this.addProduct}/>
             </Route>
+
             <Route path='/'>
-              {
-                this.state.products.map((product) => {
-                  return (
-                    <div className="product" key={product._id}>
-                      <h1>{product.name}</h1>
-                      <img src={product.img} />
-                      <Link to={"/" + product._id + '/edit'}>Edit Product</Link>
-                      <Link to={"/" + product._id}>More Details</Link>
-                    </div>
-                  )
-                })
-              }
+               <div>
+                {
+                  this.state.products.map((product) => {
+                    return (
+                      <div className="product" key={product._id}>
+                        <header>Go to Cart</header>
+                        <h1>{product.name}</h1>
+                        <img src={product.img} alt={`${this.props.name} watches`} />
+                        <h3>{product.price}</h3>
+                        <button onClick={() => this.props.cartFunction(product)}>add</button>
+                        <Link to={"/" + product._id + '/edit'}>Edit Product</Link>
+                        <Link to={"/" + product._id}>More Details</Link>
+                      </div>
+                    )
+                  })
+                }
+              </div>
             </Route>
           </Switch>
-        </Router>
+        </div>
+      </Router>
       </div>
     );
   }
